4d9c84c3dba897d4989c6357ae549ecd834bede6
Anmol <Anmol.shkl@gmail.com>
2014-11-10 21:22:20 +0530

    change register form UI add test placeholders text in inputs

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -8,7 +8,12 @@ class UserForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ('username', 'email','first_name','last_name') 
+        fields = ('username', 'email','first_name','last_name')
+        widgets = {
+            'email': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'first_name': forms.TextInput(
+                attrs={'placeholder': 'Enter description here'}),
+        }     
 
     def clean_email(self):
         email = self.cleaned_data["email"]
